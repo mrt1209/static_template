1060289bf52bcef079e7f8e1fe33bcbbb82d2679
mrt1209 <mrt1209@gmail.com>
2021-03-12 17:30:27 -0500

    b-img component not working for local assets, switching to regular <img for now

--- src/views/ShandonPreview.vue
+++ src/views/ShandonPreview.vue
@@ -4,12 +4,11 @@
       <b-navbar class="d-flex" toggleable>
         <div id="center-nav" class="d-flex mx-auto">
           <!-- <div class="mx-auto" href="#"> -->
-          <b-img
+          <img
             id="shandon-logo"
-            src="./src/assets/shandon/Shandon_logo.png"
-            fluid
-            class="justify-content-center mx-auto my-auto"
-          ></b-img>
+            src="../assets/shandon/Shandon_logo.png"
+            class="justify-content-center mx-auto my-auto fluid"
+          />
           <b-navbar-toggle class="border-0" target="navbar-toggle-collapse">
             <!-- <template #default="{ expanded }">
             <p class="h1 mb-2">
@@ -34,23 +33,22 @@
       </b-navbar>
 
       <div>
-        <b-img
-          fluid
-          class="image-one"
+        <img
+          class="image-one fluid"
           src="https://static.wixstatic.com/media/9e3e03_4bb7950793b54f369675ce6f83605e80~mv2.png/v1/fill/w_2543,h_527,al_c,q_90,usm_0.66_1.00_0.01/9e3e03_4bb7950793b54f369675ce6f83605e80~mv2.webp"
           style="opacity: 55%"
-        ></b-img>
+        />
       </div>
 
       <div
         class="d-flex"
         style="background-color: rgb(1, 127, 255); height: 45px"
       >
-        <b-img
+        <img
           height="15"
           class="mx-auto my-auto"
-          src="/most_recent_message.png"
-        ></b-img>
+          src="../assets/shandon/most_recent_message.png"
+        />
       </div>
 
       <b-row>
@@ -65,11 +63,11 @@
       </b-row>
 
       <div class="d-flex" style="background-color: black; height: 45px">
-        <b-img
+        <img
           height="15"
           class="mx-auto my-auto"
-          src="/shandon_essentials.png"
-        ></b-img>
+          src="../assets/shandon/shandon_essentials.png"
+        />
       </div>
 
       <div class="testimonial-group bg-dark">
@@ -159,20 +157,20 @@
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import { BIcon } from "bootstrap-vue";
+import Vue from 'vue';
+import { BIcon } from 'bootstrap-vue';
 export default Vue.extend({
   components: { BIcon },
   data: () => {
     return {
       selectedIcon: 1,
       imgSrc: [
-        "https://static.wixstatic.com/media/9e3e03_6318cc723de24610915c5f8e89a7500a~mv2.jpg/v1/fill/w_2543,h_544,al_c,q_85,usm_0.66_1.00_0.01/9e3e03_6318cc723de24610915c5f8e89a7500a~mv2.webp",
-        "https://static.wixstatic.com/media/9e3e03_cc728cf70e834828af156e6afab30f51~mv2.png/v1/fill/w_2543,h_544,al_c,q_90,usm_0.66_1.00_0.01/9e3e03_cc728cf70e834828af156e6afab30f51~mv2.webp",
-        "https://static.wixstatic.com/media/9e3e03_49fa7ecfa5594c30b291b685823f76b3~mv2.png/v1/fill/w_2543,h_544,al_c,q_90,usm_0.66_1.00_0.01/9e3e03_49fa7ecfa5594c30b291b685823f76b3~mv2.webp"
+        'https://static.wixstatic.com/media/9e3e03_6318cc723de24610915c5f8e89a7500a~mv2.jpg/v1/fill/w_2543,h_544,al_c,q_85,usm_0.66_1.00_0.01/9e3e03_6318cc723de24610915c5f8e89a7500a~mv2.webp',
+        'https://static.wixstatic.com/media/9e3e03_cc728cf70e834828af156e6afab30f51~mv2.png/v1/fill/w_2543,h_544,al_c,q_90,usm_0.66_1.00_0.01/9e3e03_cc728cf70e834828af156e6afab30f51~mv2.webp',
+        'https://static.wixstatic.com/media/9e3e03_49fa7ecfa5594c30b291b685823f76b3~mv2.png/v1/fill/w_2543,h_544,al_c,q_90,usm_0.66_1.00_0.01/9e3e03_49fa7ecfa5594c30b291b685823f76b3~mv2.webp'
       ],
       imgSrcTwo:
-        "https://static.wixstatic.com/media/9e3e03_b8f4e20c76cc4280bd2c8bd7ca7ece14~mv2.jpg/v1/fill/w_1920,h_309,al_c,q_85/9e3e03_b8f4e20c76cc4280bd2c8bd7ca7ece14~mv2.webp"
+        'https://static.wixstatic.com/media/9e3e03_b8f4e20c76cc4280bd2c8bd7ca7ece14~mv2.jpg/v1/fill/w_1920,h_309,al_c,q_85/9e3e03_b8f4e20c76cc4280bd2c8bd7ca7ece14~mv2.webp'
     };
   },
   methods: {},
@@ -191,8 +189,8 @@ export default Vue.extend({
 }
 
 .title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
-    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
@@ -242,13 +240,13 @@ export default Vue.extend({
     display: none;
   }
   #rotatingImg1Div::before {
-    content: url("https://static.wixstatic.com/media/9e3e03_6318cc723de24610915c5f8e89a7500a~mv2.jpg/v1/fill/w_980,h_544,al_c,q_85,usm_0.66_1.00_0.01/9e3e03_6318cc723de24610915c5f8e89a7500a~mv2.webp");
+    content: url('https://static.wixstatic.com/media/9e3e03_6318cc723de24610915c5f8e89a7500a~mv2.jpg/v1/fill/w_980,h_544,al_c,q_85,usm_0.66_1.00_0.01/9e3e03_6318cc723de24610915c5f8e89a7500a~mv2.webp');
   }
   #rotatingImg2Div::before {
-    content: url("https://static.wixstatic.com/media/9e3e03_cc728cf70e834828af156e6afab30f51~mv2.png/v1/fill/w_980,h_544,al_c,q_90,usm_0.66_1.00_0.01/9e3e03_cc728cf70e834828af156e6afab30f51~mv2.webp");
+    content: url('https://static.wixstatic.com/media/9e3e03_cc728cf70e834828af156e6afab30f51~mv2.png/v1/fill/w_980,h_544,al_c,q_90,usm_0.66_1.00_0.01/9e3e03_cc728cf70e834828af156e6afab30f51~mv2.webp');
   }
   #rotatingImg3Div::before {
-    content: url("https://static.wixstatic.com/media/9e3e03_49fa7ecfa5594c30b291b685823f76b3~mv2.png/v1/fill/w_980,h_544,al_c,q_90,usm_0.66_1.00_0.01/9e3e03_49fa7ecfa5594c30b291b685823f76b3~mv2.webp");
+    content: url('https://static.wixstatic.com/media/9e3e03_49fa7ecfa5594c30b291b685823f76b3~mv2.png/v1/fill/w_980,h_544,al_c,q_90,usm_0.66_1.00_0.01/9e3e03_49fa7ecfa5594c30b291b685823f76b3~mv2.webp');
   }
 }
 </style>
